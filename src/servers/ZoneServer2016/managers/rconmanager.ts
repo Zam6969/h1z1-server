fb7e78d678b72299219fea962ff1febbe2460c00
zam6969 <12osegaming@gmail.com>
2023-05-20 20:46:19 -0700

    rcon changes.

--- src/servers/ZoneServer2016/managers/rconmanager.ts
+++ src/servers/ZoneServer2016/managers/rconmanager.ts
@@ -11,19 +11,37 @@
 //   Based on https://github.com/psemu/soe-network
 // ======================================================================
 
-import * as http from 'http';
-import express from 'express';
-import bodyParser from 'body-parser';
-import { ZoneServer2016 } from '../zoneserver'
-import { ZoneClient2016 } from '../classes/zoneclient';
+import * as http from "http";
+import express from "express";
+import bodyParser from "body-parser";
+import { ZoneServer2016 } from "../zoneserver";
+import { ZoneClient2016 } from "../classes/zoneclient";
 
+function requireAuthorization(
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+): void {
+  const authorizationHeader = req.headers.authorization;
+  const expectedKey = process.env.RCON_PASSWORD;
 
+  if(!expectedKey) {
+    res.status(401).json({ error: "Key not set" });
+    return;
+  }
+  if (!authorizationHeader || authorizationHeader !== expectedKey) {
+    res.status(401).json({ error: "Unauthorized" });
+    return;
+  }
+
+  // Authorization succeeded, proceed to the next middleware or route handler
+  next();
+}
 
 export class RConManager {
   private app: express.Express;
   private server: http.Server;
   private zoneServer: ZoneServer2016;
-  _pw = process.env.PW ||"";
 
   constructor(zoneServer: ZoneServer2016) {
     this.zoneServer = zoneServer;
@@ -32,11 +50,12 @@ export class RConManager {
 
     // Configure body-parser middleware to parse JSON requests
     this.app.use(bodyParser.json());
+    this.app.use(requireAuthorization);
 
     // Define routes
-    this.app.post('/rcon/alert', this.handleSendAlert.bind(this));
-    this.app.post('/rcon/shutdown', this.handleShutdown.bind(this));
-
+    this.app.get("/rcon/players", this.handleGetPlayers.bind(this));
+    this.app.post("/rcon/alert", this.handleSendAlert.bind(this));
+    this.app.post("/rcon/shutdown", this.handleShutdown.bind(this));
   }
 
   public startHttpServer(port: number): void {
@@ -45,40 +64,99 @@ export class RConManager {
     });
   }
 
-  private handleSendAlert(req: express.Request, res: express.Response): void {
-    if(req.body.token == this._pw) {
-        console.log(req.body.msg)
-        // Use the ZoneServer2016 instance to send the alert
-        this.zoneServer.sendAlertToAll(`Broadcast from SYSTEM: ${req.body.msg}`);
-
-        res.json({ success: true, msg: req.body.msg });
-    } else {
-        res.status(401).json({error: 'Invalid token'});
-    }
+  private handleGetPlayers(req: express.Request, res: express.Response): void {
+    // Use the ZoneServer2016 instance to send the alert
+    var players = this.zoneServer._clients;
+    var playerList: {
+      guid: string | undefined;
+      character: { name: string  };
+      isAdmin: boolean;
+      isDebugMode: boolean;
+      HWID: string;
+      pvpStats: {
+        shotsFired: number;
+        shotsHit: number;
+        head: number;
+        spine: number;
+        hands: number;
+        legs: number;
+      };
+      clientLogs: {
+        log: string;
+        isSuspicious: boolean;
+      }[];
+      loginSessionId: string;
+      sessionId: number;
+      soeClientId: string;
+      avgPing: number;
+      permissionLevel: number;
+    }[] = [];
+    Object.values(this.zoneServer._clients).forEach(
+      (client: ZoneClient2016) => {
+        const { position, rotation } = client.character.state;
+        // client,
+        // `position: ${position[0].toFixed(2)},${position[1].toFixed(
+        //   2
+        // )},${position[2].toFixed(2)}`
+        playerList.push({
+          guid: client.guid,
+          character: {
+            name: client.character.name
+            // positon: ,
+            // rotation
+          },
+          isAdmin: client.isAdmin,
+          isDebugMode: client.isDebugMode,
+          HWID: client.HWID,
+          pvpStats: client.pvpStats,
+          clientLogs: client.clientLogs,
+          loginSessionId: client.loginSessionId,
+          sessionId: client.sessionId,
+          soeClientId: client.soeClientId,
+          avgPing: client.avgPing,
+          permissionLevel: client.permissionLevel,
+        });
+      }
+    );
+    res.json({ success: true, players: playerList });
   }
-  private async handleShutdown(req: express.Request, res: express.Response): Promise<void> { 
-    if(req.body.token == this._pw) {
-        // Use the ZoneServer2016 instance to send the alert
-        this.zoneServer.sendAlertToAll(`Broadcast from SYSTEM: Server is shutting down NOW.`);
-        this.zoneServer.sendDataToAll("WorldShutdownNotice", {
-            timeLeft: 0,
-            message: "Zone is shutting down.",
-          });
-          await this.zoneServer.saveWorld();
-          setTimeout(() => {
-            Object.values(this.zoneServer._clients).forEach((client: ZoneClient2016) => {
-                this.zoneServer.sendData(client as any, "CharacterSelectSessionResponse", {
-                  status: 1,
-                  sessionId: client.loginSessionId,
-                });
-              });
-          }, 2000);
-          setTimeout(() => {
-            process.exit(0);
-          }, 6000);
-        res.json({ success: true, msg: 'zone shut down' });
-    } else {
-        res.status(401).json({error: 'Invalid token'});
-    }
+  private handleSendAlert(req: express.Request, res: express.Response): void {
+    if (!req.body.msg) res.json({ success: false, msg: "no message provided" });
+    // Use the ZoneServer2016 instance to send the alert
+    this.zoneServer.sendAlertToAll(`Broadcast from SYSTEM: ${req.body.msg}`);
+
+    res.json({ success: true, msg: req.body.msg });
+  }
+  private async handleShutdown(
+    req: express.Request,
+    res: express.Response
+  ): Promise<void> {
+    // Use the ZoneServer2016 instance to send the alert
+    this.zoneServer.sendAlertToAll(
+      `Broadcast from SYSTEM: Server is shutting down NOW.`
+    );
+    this.zoneServer.sendDataToAll("WorldShutdownNotice", {
+      timeLeft: 0,
+      message: "Zone is shutting down.",
+    });
+    await this.zoneServer.saveWorld();
+    setTimeout(() => {
+      Object.values(this.zoneServer._clients).forEach(
+        (client: ZoneClient2016) => {
+          this.zoneServer.sendData(
+            client as any,
+            "CharacterSelectSessionResponse",
+            {
+              status: 1,
+              sessionId: client.loginSessionId,
+            }
+          );
+        }
+      );
+    }, 2000);
+    setTimeout(() => {
+      process.exit(0);
+    }, 6000);
+    res.json({ success: true, msg: "zone shut down" });
   }
 }
